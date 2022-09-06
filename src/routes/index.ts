ffc6f1e3758a4249f31932c5a39715422b43d3c8
Leonardo Melo <leoomelo@gmail.com>
2022-09-06 17:01:32 -0300

    feat: try to configure ts-node

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,6 @@ import { Router } from 'express';
 
 import * as HomeController from '../controllers/homeController';
 import * as InfoController from '../controllers/infoController';
-import * as UserController from '../controllers/userController';
 
 const router = Router();
 
@@ -10,9 +9,5 @@ router.get('/', HomeController.home);
 
 router.get('/contato', InfoController.contato);
 router.get('/sobre', InfoController.sobre);
-
-router.get('/nome', UserController.nome);
-router.get('/idade', UserController.idadeForm);
-router.post('/idade-resultado', UserController.idadeAction);
 
 export default router;
--- src/server.ts
+++ src/server.ts
@@ -22,4 +22,4 @@ server.use((req: Request, res: Response)=>{
     res.status(404).send('Página não encontrada!');
 });
 
-server.listen(process.env.PORT || '3000');
+server.listen(process.env.PORT);
