3d0700855d4ca6088bd45fd75ec4cbb8b0bb2c1c
Leonardo Melo <leoomelo@gmail.com>
2022-11-05 09:03:55 -0300

    feat: adjustments json bets

--- src/models/Bet.ts
+++ src/models/Bet.ts
@@ -2,17 +2,17 @@ import { Schema, Model, model, connection } from 'mongoose'
 
 type BetType = {
   team1: string,
-  teamScore1: number,
+  oficialTeamScore1: number,
   team2: string,
-  teamScore2: number,
+  oficialTeamScore2: number,
   userEmail: string
 }
 
 const schema = new Schema<BetType>({
   team1: { type: String, required: true },
-  teamScore1: { type: Number, required: true },
+  oficialTeamScore1: { type: Number, required: true },
   team2: { type: String, required: true },
-  teamScore2: { type: Number, required: true },
+  oficialTeamScore2: { type: Number, required: true },
   userEmail: { type: String, required: true }
 })
 
